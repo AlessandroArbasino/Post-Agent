ddab598fd089ddb821825414d7540df285a4b5b5
Alessandro Arbasino <alessandroarbasino99@gmail.com>
2025-11-24 20:44:50 +0100

    removed comments into test video api

--- app/api/video/test/route.js
+++ app/api/video/test/route.js
@@ -6,9 +6,8 @@ export async function GET() {
     const asset = await uploadAssetToHeygen("https://res.cloudinary.com/dwpapdlgk/image/upload/v1763824440/dailypost/2025-11-22/bghm3xkxlhxbuzggtvod.webp")
     const speechText = await generateSpeech({prompt:"Even in the everyday, hope finds a way to shine through. ✨ Feeling this little spark of magic today. What are you holding onto hope for right now? Let me know in the comments! 👇"});
     const speech =speechText.geminiResponse;
-    //const videoId = await generateVideo({backgroundAssetId:asset.id,backgroundUrl:asset.url,speechText:speech});
-    //return Response.json({ ok:true,video_id: videoId });
-    return Response.json({ ok:true,asset,speech });
+    const videoId = await generateVideo({backgroundAssetId:asset.id,backgroundUrl:asset.url,speechText:speech});
+    return Response.json({ ok:true,video_id: videoId });
   } catch (err) {
     const message = err instanceof Error ? err.message : 'Unknown error';
     return Response.json({ error: message }, { status: 500 });
