b0e74e6e0d2a0e154dd632474f0ec49472123852
Alessandro Arbasino <alessandroarbasino99@gmail.com>
2025-12-21 14:24:15 +0100

    remove new URL

--- app/services/cloudinary/uploader.js
+++ app/services/cloudinary/uploader.js
@@ -99,11 +99,11 @@ const labeledImageUrl = async (url, label, opts = {}) => {
     },
   ]
 
-  const urlTransformed = new URL(cloudinary.url(url, {
+  const urlTransformed = cloudinary.url(url, {
     type: 'fetch',
     secure: false,
     transformation
-  }));
+  });
 
   const uploadResult = await uploadToCloudinary(urlTransformed, { folder: 'annotated', publicId: label });
 
